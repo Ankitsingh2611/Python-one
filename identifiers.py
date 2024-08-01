dff4a7c6bfb101e28b3e937506d32738a805025c
Ankit Kumar Singh <ankittchauhan937@gmail.com.com>
2024-08-01 17:34:36 +0530

    first commit

---
+++ identifiers.py
@@ -0,0 +1,17 @@
+#valid identifiers
+age = 24
+name = "Ankit"
+last_name = "Singh"
+_id = '23x5342A'
+name2 = "Shivam"
+
+
+#invalid identifiers
+2name = "Shivam"
+marks$ = 98
+class = "Hello"
+
+
+print(age)
+print(name)
+
